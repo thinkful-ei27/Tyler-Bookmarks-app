dcb7cd761c6c65b69de90b310094f5a27015ddef
Tyler Crabb <tydacrabb@gmail.com>
2018-12-13 09:31:05 -0800

    added caputure user input and get json from it

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
+$.fn.extend({
+  serializeJson: function () {
+    const formData = new FormData(this[0]);
+    const jsonObj = {};
+    formData.forEach((val, key) => {
+      jsonObj[key] = val;
+    });
+    return JSON.stringify(jsonObj);
+  }
+});
+
 function captureInput(){
   $('.add-a-bookmark').submit(event => {
     event.preventDefault();
-    const testData = $(event.target).serializeArray();
+    const testData = $(event.target).serializeJson();
     console.log(testData);
   });
 }
